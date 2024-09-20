217254fb226f327e401f444dea66d6993cff8441
Carl Beattie <12beattiecastp@gmail.com>
2024-09-21 00:11:10 +0100

    test init

---
+++ pcparts.py
@@ -0,0 +1,23 @@
+from quart import Quart, render_template
+from pcpartpicker import API
+import json
+
+app = Quart(__name__)
+pcpartpickerapi = API()
+
+@app.route("/")
+async def get_home_page():
+    return await render_template("./index.html")
+
+@app.route("/amd/<string:offset1>")
+async def get_amd_cpu(offset1=""):
+    result = await pcpartpickerapi.retrieve("cpu")
+    resultJson = json.loads(result.to_json())
+    for cpu in resultJson["cpu"]:
+        if cpu["brand"] == "AMD" and cpu["model"] == offset1:
+            return cpu
+    return json.dumps({ "error": "not found" })
+
+if __name__ == "__main__":
+    app.run()
+
